b7742306a74b439865484537c5f1ce80fa4e2cb8
Alejandro <alejandro.garcia.dlf@gmail.com>
2024-04-22 15:43:46 +0200

    Added database

---
+++ app/src/main/java/com/example/myfinance/MyFinanceDatabase.kt
@@ -0,0 +1,26 @@
+package com.example.myfinance
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Profile::class, Account::class, Transaction::class, TransactionType::class], version = 1)
+abstract class MyFinanceDatabase: RoomDatabase() {
+    companion object {
+        @Volatile
+        private var INSTANCE: MyFinanceDatabase? = null
+
+        fun getDatabase(context: Context): MyFinanceDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    MyFinanceDatabase::class.java,
+                    context.resources.getString(R.string.database_name)
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
