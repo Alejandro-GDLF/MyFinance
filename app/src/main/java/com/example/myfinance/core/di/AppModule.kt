6f6544c6b2a6d00075f066d08f9ccc0b03d61c5a
Alejandro <alejandro.garcia.dlf@gmail.com>
2024-05-11 16:50:27 +0200

    Added dependency injection for database

---
+++ app/src/main/java/com/example/myfinance/core/di/AppModule.kt
@@ -0,0 +1,27 @@
+package com.example.myfinance.core.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.myfinance.R
+import com.example.myfinance.core.database.MyFinanceDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideMyFinanceDatabase(@ApplicationContext appContext: Context): MyFinanceDatabase {
+        return Room.databaseBuilder(
+            appContext,
+            MyFinanceDatabase::class.java,
+            appContext.getString(R.string.database_name)
+        ).build()
+    }
+}
