24ae924d46108fbdb456d0aaf830b45b6c648343
Alejandro <alejandro.garcia.dlf@gmail.com>
2024-05-30 16:23:08 +0200

    Added some comments to explain the reason of the creation of use_cases

--- app/src/main/java/com/example/myfinance/account/domain/use_case/CreateNewAccountUseCase.kt
+++ app/src/main/java/com/example/myfinance/account/domain/use_case/CreateNewAccountUseCase.kt
@@ -7,6 +7,14 @@ import com.example.myfinance.profile.domain.Profile
 import java.time.LocalDateTime
 import javax.inject.Inject
 
+/**
+ * @brief Use case for creating a new account.
+ *
+ * This use case is created for future-proof operation, being more flexible to changes in domain
+ * requirements or validations.
+ *
+ * Also, abstracts the presentation layer from the instantiation of domain objects.
+ */
 class CreateNewAccountUseCase @Inject constructor(
     private val accountFactory: AccountFactory,
     private val accountRepository: AccountRepository
--- app/src/main/java/com/example/myfinance/profile/domain/use_cases/CreateNewProfileUseCase.kt
+++ app/src/main/java/com/example/myfinance/profile/domain/use_cases/CreateNewProfileUseCase.kt
@@ -5,6 +5,14 @@ import com.example.myfinance.profile.domain.ProfileFactory
 import com.example.myfinance.profile.domain.ProfileRepository
 import javax.inject.Inject
 
+/**
+ * @brief Use case for creating a new profile.
+ *
+ * This use case is created for future-proof operation, being more flexible to changes in domain
+ * requirements or validations.
+ *
+ * Also, abstracts the presentation layer from the instantiation of domain objects.
+ */
 class CreateNewProfileUseCase @Inject constructor(
     private val profileFactory: ProfileFactory,
     private val profileRepository: ProfileRepository
